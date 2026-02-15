080a5bebf2d645e2609f2c7141bd4565f32bef2d
alissonpaulinelli1 <alissonpaulinelli1@hotmail.com>
2026-02-15 16:30:42 -0600

    Update illustrate.js

--- api/illustrate.js
+++ api/illustrate.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, route: "/api/illustrate" });
+  res.status(200).json({
+    ok: true,
+    route: "/api/illustrate"
+  });
 }
